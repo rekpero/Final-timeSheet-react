cacb88a42481c65910ab4be070f36bdc7bd3ee8f
Paras Kumar <parakum1@in.ibm.com>
2019-11-08 09:47:38 +0530

    start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppRouter from './components/appRouter';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
