32a3de38c41d1046d670f66db0593fe401aee563
sherrynsfw <31062493+sherrynsfw@users.noreply.github.com>
2019-11-11 12:45:45 +0530

    settings ui

--- src/components/appRouter.tsx
+++ src/components/appRouter.tsx
@@ -5,6 +5,7 @@ import App from "../App";
 import Login from '../pages/logincomponent'
 import Register from "../pages/registercomponent";
 import Dashboard from "../pages/dashboardcomponent";
+import WorkspaceSetting from "../pages/workspacesetting";
 
 
 import New from "../pages/new";
@@ -17,7 +18,7 @@ const AppRouter: React.FC = () => {
   return (
     <Router>
       <App>
-      
+      <Route exact path="/settings" component={WorkspaceSetting} />
          <Route exact path="/" component={Login} />
   <Route exact path="/register" component={Register}/>     
   <Route path="/dash" component={Dashboard} />
