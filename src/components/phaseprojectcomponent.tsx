d5d2bd991049eea83932166f660dd786491cc253
sherrynsfw <31062493+sherrynsfw@users.noreply.github.com>
2019-11-14 16:31:03 +0530

    phases project

--- src/components/phaseprojectcomponent.tsx
+++ src/components/phaseprojectcomponent.tsx
@@ -7,22 +7,23 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { Checkbox } from "@material-ui/core";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles({
-    root: {
-        width: "70%",
-        overflowX: "auto",
-        marginLeft: "15%"
-    },
-        table: {
-        maxWidth: "100%"
-    }
+root: {
+width: "70%",
+overflowX: "auto",
+marginLeft: "15%"
+},
+table: {
+maxWidth: "100%"
+}
 });
 
 function createData(cbox: boolean, name: string, budget:number, project: number) {
-    return { cbox, name, budget, project };
+return { cbox, name, budget, project };
 }
-
 var checkboxValue= false;
 var rows = [
 createData(false, "AT&T Dev", 14, 159),
@@ -31,73 +32,79 @@ createData(true, "Metlife Marketing", 13, 262)
 ];
 
 const PhaseProjectComponent: React.FC<{}> = (props: any) => {
-    const classes = useStyles();
-    const [stateRows, setStateRows] = useState(rows)
+const classes = useStyles();
+const [stateRows, setStateRows] = useState(rows)
 
-    const handleChange=(e: any, i: number) => {
-    console.log(i, stateRows[i].cbox)
-    stateRows[i].cbox = !stateRows[i].cbox
-    setStateRows(stateRows)
-    console.log(i, stateRows)
-    }
+const handleChange=(e: any, i: number) => {
+console.log(i, stateRows[i].cbox)
+stateRows[i].cbox = !stateRows[i].cbox
+setStateRows(stateRows)
+console.log(i, stateRows)
+}
 
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                <TableCell>
-                Active
-                </TableCell>
-                <TableCell>
-                Name
-                </TableCell>
-                <TableCell>
-                Category
-                </TableCell>
-                <TableCell align="right">
-                Budget
-                </TableCell>
-                <TableCell align="right">
-                Time Tracked
-                </TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                <TableRow>
-                </TableRow>
-                {stateRows.map((row, i) => (
-                <TableRow
-                key={row.name}
-                hover
-                onClick={e => {
-                }}
-                >
+return (
+<Paper className={classes.root}>
+<Table className={classes.table} aria-label="simple table">
+<TableHead>
+<TableRow>
+<TableCell>
+Active
+</TableCell>
+<TableCell>
+Name
+</TableCell>
+<TableCell>
+Category
+</TableCell>
+<TableCell >
+Budget
+</TableCell>
+<TableCell >
+Time Tracked
+</TableCell>
+<TableCell >
+
+</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{stateRows.map((row, i) => (
+<TableRow
+key={row.name}
+hover
+onClick={e => {
+}}
+>
 
 
 
-                <TableCell>
-                <Checkbox
-                value={row.cbox}
-                checked={row.cbox}
-                inputProps={{ 'aria-labelledby': "checkbox in table" }}
-                color= "primary"
-                onClick={e => handleChange(e, i)}
-                />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                {row.name}
-                </TableCell>
-                <TableCell >
-                askjdnek
-                </TableCell>
-                <TableCell align= "right">{row.budget}</TableCell>
-                <TableCell align="right">{row.project}</TableCell>
-                </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+<TableCell>
+<Checkbox
+value={row.cbox}
+checked={row.cbox}
+inputProps={{ 'aria-labelledby': "checkbox in table" }}
+color= "primary"
+onClick={e => handleChange(e, i)}
+/>
+</TableCell>
+<TableCell component="th" scope="row">
+{row.name}
+</TableCell>
+<TableCell >
+askjdnek
+</TableCell>
+<TableCell >{row.budget}</TableCell>
+<TableCell >{row.project}</TableCell>
+<TableCell>
+<IconButton aria-label="delete">
+<DeleteIcon />
+</IconButton>
+</TableCell>
+</TableRow>
+))}
+</TableBody>
+</Table>
+</Paper>
+);
 };
 export default PhaseProjectComponent;
